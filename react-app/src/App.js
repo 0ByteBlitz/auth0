b1d769b9be548f453fc58c7f499880b8f119b299
Mukesh Ghimire <samu.mg60@gmail.com>
2018-10-16 23:20:09 +0530

    adding react-app

---
+++ react-app/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
+import Question from './Question/Question';
+import Questions from './Questions/Questions';
+import { Route, withRouter } from 'react-router-dom';
+import Callback from './Callback';
+import NewQuestion from './NewQuestion/NewQuestion';
+import SecuredRoute from './SecuredRoute/SecuredRoute';
+import auth0Client from './Auth';
+
+class App extends Component {
+   constructor(props){
+     super(props);
+     this.state ={
+       checkingSession: true,
+     }
+   }
+   async componentDidMount() {
+     if (this.props.location.pathname === '/callback') return console.log('no path');
+     try {
+       console.log('starting silentAuth');
+       await auth0Client.silentAuth();
+       console.log('silentAuth done');
+       this.forceUpdate();
+     } catch (err) {
+       if (err.error === 'login_required') return;
+       console.log(err.error);
+     }
+     this.setState({checkingSession:false});
+   }
+  
+  
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <Route exact path='/' component={Questions}/>
+        <Route exact path='/question/:questionId' component={Question}/>
+        <Route exact path='/callback' component={Callback}/>
+        <SecuredRoute path='/new-question' component={NewQuestion} checkingSession={this.state.checkingSession} />
+      </div>
+    );
+  }
+}
+
+export default withRouter(App);
