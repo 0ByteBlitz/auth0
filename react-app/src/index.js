0e3acf0642f6649d26366ab70f5dc74687cda585
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-18 09:40:59 -0300

    fixing formatting;

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <BrowserRouter> 
-<div>
-< App / >
-</div>
-</BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <App/>
+        </div>
+    </BrowserRouter>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
